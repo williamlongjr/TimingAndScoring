5dac58d3d5ebb06d57e8fb3ec8ce0a1a7def2496
William Long <william.long@becu.org>
2019-10-04 16:44:54 -0700

    adding riders and places

---
+++ TimingAndScoring/TimingAndScoring/Data/Rider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimingAndScoring.Data
+{
+    public class Rider
+    {
+        public int CurrentPlace { get; set; }
+        public int Number { get; set; }
+        public string LastName { get; set; }
+        public string SecondsDown { get; set; }
+        public int LapsDown { get; set; }
+        public double LastLapTime { get; set; }
+        public int SeasonPoints { get; set; }
+        public int SeasonPosition { get; set; }
+    }
+}
